f97baea89976e7ede95b5eecd864c9fb7c215383
Phat Vuong <vvp301097@gmail.com>
2024-10-08 21:49:55 +0700

    Initial Commit

---
+++ Custom Video Recorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Custom Video Recorder
+//
+//  Created by Phat Vuong Vinh on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom Video Recorder/Custom_Video_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Video_RecorderApp.swift
+//  Custom Video Recorder
+//
+//  Created by Phat Vuong Vinh on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Video_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
